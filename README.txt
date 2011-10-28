4d3b011c893b96aa9f5f5f0d0f0a487c49affa2c
Henrik Almer <henrik@agoodid.se>
2011-10-28 17:39:51 +0200

    Initial commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+.vim directory of file type configurations and plugins
---
+++ test.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+import os
+import sys
+
+
+class MyClass():
+    pass
+
+
+def cool():
+    """docstring for cool"""
+    pass
+
+
+def uncool():
+    """docstring for uncool"""
+    pass
